baca1a05e69c1924cff7099aa7cf60d23a30983d
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-01-07 21:21:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+   const developers = [{id:1,name:"john",age:"18",profession:"developer"},{id:2, name:"jack",age:"20", profession:"developer"},{id:3, name:"karen", age:"19",profession:"admin"}];
+  const developerNames = developers.map(developer => {
+    if (developer.profession === 'developer') {
+      return developer.name;
+    }
+  });
+  console.log(developerNames);
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+   const developers = [{id:1,name:"john",age:"18",profession:"developer"},{id:2, name:"jack",age:"20", profession:"developer"},{id:3, name:"karen", age:"19",profession:"admin"}];
+  developers.forEach(developer => {
+    if (developer.profession === 'developer') {
+      console.log(developer.name);
+    }
+  });
+  
 }
 
 function addData() {
   //Write your code here
+   const newEmployee = {id:4,name:"susan",age:"20",profession:"intern"};
+  const developers = [{id:1,name:"john",age:"18",profession:"developer"},{id:2, name:"jack",age:"20", profession:"developer"},{id:3, name:"karen", age:"19",profession:"admin"}];
+  developers.push(newEmployee);
+  console.log(developers);
 }
 
 function removeAdmin() {
   //Write your code here
+   const developers = [{id:1,name:"john",age:"18",profession:"developer"},{id:2, name:"jack",age:"20", profession:"developer"},{id:3, name:"karen", age:"19",profession:"admin"}];
+  const updatedDevelopers = developers.filter(developer => developer.profession !== 'admin');
+  console.log(updatedDevelopers);
 }
 
 function concatenateArray() {
   //Write your code here
+   const developers = [{id:1,name:"john",age:"18",profession:"developer"},{id:2, name:"jack",age:"20", profession:"developer"},{id:3, name:"karen", age:"19",profession:"admin"}];
+  const newDevelopers = [{id:4,name:"tim",age:"23",profession:"developer"},{id:5, name:"peter",age:"30", profession:"developer"},{id:6, name:"jane", age:"25",profession:"admin"}];
+  const allDevelopers = developers.concat(newDevelopers);
+  console.log(allDevelopers);
 }
